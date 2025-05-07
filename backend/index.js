8f6e75c9781088038051843b70a51cc524dea0d7
Simone Papa <simonepapa24@gmail.com>
2025-05-07 10:31:46 +0200

    Added backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const PORT = 3000;
+
+// Connessione al database SQLite
+const db = new sqlite3.Database('database.db', (err) => {
+  if (err) return console.error(err.message);
+  console.log('Connected to SQLite DB.');
+});
+
+// Endpoint per leggere la tabella "users"
+app.get('/scans', (req, res) => {
+  const sql = 'SELECT * FROM scans';
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Error while reading from DB.' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
